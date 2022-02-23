66777fc958dde7ea8b9cb6b2a940f0452d117c81
Emil Hertz Degn <emilhd@protonmail.com>
2022-02-23 15:17:23 +0100

    Add project files.

---
+++ SWT-DoorControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWT_DoorControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
