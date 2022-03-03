845097278da7777ee942cdbca4915f5e4bc05a3f
Emil Hertz Degn <emilhd@protonmail.com>
2022-03-03 13:35:14 +0100

    started basis for tests

---
+++ DoorTest/DoorClassTestClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using NUnit.Framework;
+using SWT_DoorControl;
+
+namespace DoorTest
+{
+    class DoorClassTestClass
+    {
+
+        private IDoorControl _doorControl;
+        private IUserValidation _userValidation;
+        private IDoor _uut;
+        private IEntryNotification _entryNotification;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userValidation = Substitute.For<IUserValidation>();
+            _doorControl = Substitute.For<IDoorControl>();
+            _entryNotification = Substitute.For<IEntryNotification>();
+            _uut = new Door();
+        }
+
+        [Test]
+        public void TestOpen()
+        {
+            Assert.Pass();
+        }
+
+        public void TestClose()
+        {
+            Assert.Pass();
+        }
+
+        public void TestOpen()
+        {
+            Assert.Pass();
+        }
+
+        public void TestOpen()
+        {
+            Assert.Pass();
+        }
+
+
+
+
+
+
+
+    }
+}
