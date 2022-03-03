0d6528e68fec4db46448ff2b2ba5a1758c16a3c5
Kasper <202006328@post.au.dk>
2022-03-03 13:27:02 +0100

    Tested DoorClose and fixed bug.

--- DoorTest/UnitTest1.cs
+++ DoorTest/UnitTest1.cs
@@ -45,5 +45,17 @@ namespace DoorTest
             _uut.RequestEntry(Arg.Any<int>());
             Assert.That(_uut._isValid,Is.EqualTo(isValid));
         }
+
+        [TestCase(true,true)]
+        public void DoorClose_Test(bool isDoorOpen, bool isValid)
+        {
+            _uut._isDoorOpen = isDoorOpen;
+            _uut._isValid = isValid;
+
+            _uut.DoorClosed();
+
+            Assert.That(_uut._isDoorOpen,Is.EqualTo(false));
+            Assert.That(_uut._isValid,Is.EqualTo(false));
+        }
     }
 }
